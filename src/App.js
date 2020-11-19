20fa26fb3a38534746fe6a19f485932e9305aa5b
HeatherAckerman <hla20115213@gmail.com>
2020-11-19 09:22:20 -0600

    added imports to app.js and index.js

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+//import components
+
+function App() {
+    return; //return components 
+}
+
+export default App;
