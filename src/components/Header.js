e665d85a7ac8b755dca240b9416d0b7eb705e681
HeatherAckerman <hla20115213@gmail.com>
2020-11-19 18:29:52 -0600

    added some styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,13 +4,10 @@ class Header extends Component {
     render() {
         return (
           <div style={{textAlign: "center"}}>
-            
-            <div className="jumbotron" style={{background: "navy"}}>
-            <div style={{fontWeight: 700}}>
+           <div className="jumbotron" style={{background: "navy"}}>
             <h1 style={{color: "white"}}>
               Employee Directory
             </h1>
-          </div>
           </div>
           </div>
         )
